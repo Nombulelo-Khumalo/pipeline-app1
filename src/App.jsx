03d3c12444ad8cd6ee26a16618fb250fdb0fe22d
Nombulelo Khumalo <nombulelokhumalo48@gmail.com>
2025-04-10 11:04:37 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+  
+  return (
+    <div className="text-center">
+      <h1 className=" text-5xl mt-5 font-extrabold">Congratulations 🎉🎉🎉</h1>
+      <h2 className="mt-9 font-bold text-3xl">You have successfully completed your CI/CD pipeline</h2>
+      
+    </div>
+  )
+}
+
+export default App
